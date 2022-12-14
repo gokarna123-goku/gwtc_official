71fe60d2265280539badaf2a4ebf73af6c4d9b56
gokarna123-goku <agokarna444@gmail.com>
2022-12-14 11:45:13 +0545

    contact form

--- gwtc/models.py
+++ gwtc/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    fullname = models.CharField(max_length=50)
+    email = models.EmailField()
+    message = models.CharField(max_length=255)
+    msg_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.fullname
+
+# 
