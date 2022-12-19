0fd469a203b4bf8293b659d3dd90b87e5f30008f
Suraj <suraj12@gmail.com>
2022-12-19 11:30:30 +0545

    work on poetfolioview section

--- gwtc/views.py
+++ gwtc/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from django.views import generic
 from .forms import ContactForm
 from django.contrib import messages
+from .models import Portfolio, PortfolioCategory
 
 # Create your views here.
 class Index(generic.CreateView):
@@ -15,7 +16,27 @@ class Index(generic.CreateView):
             messages.success(request, "Your Form has been submitted successfully!")
             return redirect('home')
         return render(request, self.template_name, {'form':form})
+    
+    def get(self, request, *args, **kwargs):
+        portfolio = Portfolio.objects.all()[:4]
+        context = {
+            'portfolio' : portfolio,
+        }
+        return render(request, self.template_name, context)
 
 
-class PortfolioView(generic.TemplateView):
+class PortfolioView(generic.ListView):
     template_name = 'portfolio/portfolio.html'
+    def get(self, request, *args, **kwargs):
+        portfolio = Portfolio.objects.all()
+        cat_id = self.kwargs.get('pk')
+        print(cat_id, " Category Id")
+        portfolio_category = PortfolioCategory.objects.all()[:4]
+        print(portfolio_category, " Category Name")
+        context = {
+            'portfolio' : portfolio,
+            'portfolio_category' : portfolio_category,
+        }
+        return render(request, self.template_name, context)
+
+
